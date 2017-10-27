4f7151a67c28c9559c9d3a5317d7cde2fffcff0f
botuliambr <33078224+botuliambr@users.noreply.github.com>
2017-10-27 18:01:59 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
 client.on('message', (message) => {
   console.log(message.channel.id);
@@ -28,15 +23,3 @@ client.on('message', (message) => {
     }
   }
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
