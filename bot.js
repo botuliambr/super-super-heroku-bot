e1b02b0ea229f35d14fe37135d838494fc93d842
botuliambr <33078224+botuliambr@users.noreply.github.com>
2017-12-04 10:16:53 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,4 @@
 const Discord = require('discord.js');
-const music = require('discord.js-music-v11');
-const YoutubeDL = require('youtube-dl');
-const ytdl = require('ytdl-core');
 const bot = new Discord.Client();
 
 bot.on('message', (msg) => {
@@ -12,16 +9,7 @@ bot.on('message', (msg) => {
             description: "O Bot só funciona no grupo."
           }});
         return;
-    }
-
-music(Bot, {
-	prefix: '-',       // Prefix of '-'.
-	global: false,     // Server-specific queues.
-	maxQueueSize: 10,  // Maximum queue size of 10.
-	clearInvoker: true, // If permissions applicable, allow the bot to delete the messages that invoke it (start with prefix)
-    channel: 'music'   // Name of voice channel to join. If omitted, will instead join user's voice channel.
-});
-	
+    }	
     const message = msg.content.toLowerCase();
     if(message.startsWith('!chk')) {
         Check(msg);
