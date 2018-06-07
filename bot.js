453d6c7da3321fd02b475f9635d3de48d0b7aa73
botuliambr <33078224+botuliambr@users.noreply.github.com>
2018-06-07 19:13:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-client.on('guildMemberAdd', member => {
+bot.on('guildMemberAdd', member => {
     const channel = getDefaultChannel(member.guild);
     member.addRole(member.guild.roles.find("name", "MEMBROS"));
 
