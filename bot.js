6ece3130d7ee45d1e58005feb386b5f99ba9febd
botuliambr <33078224+botuliambr@users.noreply.github.com>
2018-06-08 23:08:48 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ return message.reply("** O LIMITE DE CARTÕES É __10__**");
     function statusSender(message) {
         var msg = message.content;
         var msge = msg.replace("!status", "");
-          Request("http://thelimaochecker.tk/painel/full/status, function(result) {
+          Request("http://thelimaochecker.tk/painel/full/status.php", function(result) {
                 message.channel.send(result);
           });
       }
